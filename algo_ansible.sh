a9f1a7303edb1a329f25a92562615926f3d9fbdd
RW Martin <rw-martin@users.noreply.github.com>
2021-02-28 22:41:55 +0800

    Update algo_ansible.sh

--- algo_ansible.sh
+++ algo_ansible.sh
@@ -4,4 +4,4 @@ cd /opt/algo
   . .venv/bin/activate
 
 wget -O /opt/algo/rw-playbook.yml https://raw.githubusercontent.com/rw-martin/algo-setup/main/rw-playbook.yml
-ansible-playbook rw-playbook.yml
+ansible-playbook rw-playbook.yml -v
