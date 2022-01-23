5dfe51469469424dd3b0ff979a1a0c05979f0696
RW Martin <rw-martin@users.noreply.github.com>
2022-01-23 18:34:06 +0800

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -2,4 +2,4 @@ export USERS=linksys,laptop,phonea,phoneb,surface,himedia
 cd /opt/algo
 . .venv/bin/activate
 wget -O /dev/shm/reset.yml https://raw.githubusercontent.com/rw-martin/algo-setup/main/reset.yml
-ansible-playbook /devrshm/reset.yml 
+ansible-playbook /dev/shm/reset.yml 
